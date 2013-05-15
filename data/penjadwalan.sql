6726e191cbade7471e135e8c9b7c9cd32426b4da
erwinmaruli <erwinmaruli@live.com>
2013-05-15 15:13:11 +0700

    more data

--- data/penjadwalan.sql
+++ data/penjadwalan.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 11, 2013 at 03:24 AM
+-- Generation Time: May 15, 2013 at 10:12 AM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -83,6 +83,28 @@ CREATE TABLE IF NOT EXISTS `dosen` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `fakultas`
+--
+
+CREATE TABLE IF NOT EXISTS `fakultas` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `fakultas` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `fakultas`
+--
+
+INSERT INTO `fakultas` (`id`, `fakultas`) VALUES
+(1, 'Ekonomi'),
+(2, 'Teknologi Informasi dan Komunikasi'),
+(3, 'Komunikasi'),
+(4, 'Seni dan Design');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `mata_kuliah`
 --
 
@@ -94,7 +116,63 @@ CREATE TABLE IF NOT EXISTS `mata_kuliah` (
   `sks` int(10) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `mata_kuliah_code` (`mata_kuliah_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=54 ;
+
+--
+-- Dumping data for table `mata_kuliah`
+--
+
+INSERT INTO `mata_kuliah` (`id`, `mata_kuliah`, `mata_kuliah_code`, `praktek`, `sks`) VALUES
+(1, 'Pengantar Akuntansi 1', 'EA100', 0, 3),
+(2, 'Pengantar Akuntansi 2', 'EA201', 0, 3),
+(3, 'Ekonomi Mikro', 'EM170 ', 0, 3),
+(4, 'Ekonomi Makro', 'EM270', 0, 3),
+(5, 'Dasar-dasar Bisnis', 'EM100', 0, 3),
+(6, 'Dasar-dasar Manajemen', 'EM201', 0, 3),
+(7, 'Statistik Bisnis 1', 'EM281', 0, 3),
+(9, 'Statistik Bisnis 2', 'EM382', 0, 3),
+(10, 'Manajemen Keuangan 1', 'EM330', 0, 3),
+(11, 'Manajemen Keuangan 2', 'EM431', 0, 3),
+(12, 'Matematika Bisnis', 'EM180', 0, 3),
+(13, 'Manajemen Pemasaran', 'EM320', 0, 3),
+(14, 'Kalkulus 1', 'SK101', 0, 3),
+(15, 'Kalkulus 2', 'SK201', 0, 3),
+(16, 'Aljabar Linear', 'SK202', 0, 3),
+(17, 'Sistem Digital', 'SK211', 0, 3),
+(18, 'Probabilitas dan Multimedia Statistika', 'SK304', 0, 3),
+(19, 'Jaringan Komputer', 'SK430', 0, 3),
+(20, 'Jaringan Komputer', 'SK430P', 1, 3),
+(21, 'Keamanan Komputer dan Jaringan', 'SK532', 0, 3),
+(22, 'Sistem Terdistribusi', 'SK734', 0, 3),
+(23, 'Manajemen Operasional', 'SI214', 0, 3),
+(24, 'Sistem Informasi Manajemen', 'SI301', 0, 3),
+(25, 'Konsep Sistem Operasi', 'SI325', 0, 3),
+(26, 'Pengantar Teknologi Internet', 'TI330', 0, 3),
+(27, 'Pemograman Web', 'TI532', 0, 3),
+(28, 'Algoritma dan Pemrograman', 'TI100', 0, 3),
+(29, 'Pengantar Teknologi Multimedia', 'TI110', 0, 3),
+(30, 'Metode Perancangan Program', 'TI220', 0, 3),
+(31, 'Pemrograman Sistem Mobile', 'TI735', 0, 3),
+(32, 'Sistem Operasi', 'TI405', 0, 3),
+(33, 'Pengantar Ilmu Komunikasi', 'IK100', 0, 3),
+(34, 'Critical & Creative Thinking', 'IK112', 0, 3),
+(35, 'Teori Komunikasi', 'IK201', 0, 3),
+(36, 'Perkembangan Teknologi Komunikasi', 'IK202', 0, 3),
+(37, 'Komunikasi Antar Pribadi', 'IK203', 0, 3),
+(38, 'Multimedia Laboratory', 'IK214', 0, 3),
+(39, 'Pengantar Public Relations', 'PR210', 0, 3),
+(40, 'Creative Writing', 'IK354', 0, 3),
+(41, 'Komunikasi Interpersonal', 'IK402', 0, 3),
+(42, 'Drawing Principles', 'DA110', 0, 3),
+(43, 'Illustration', 'DA213', 0, 3),
+(44, 'West Art History', 'DV113', 0, 3),
+(45, 'East & Indonesian Art History', 'DV132', 0, 3),
+(46, 'Academic Writing', 'DA501', 0, 3),
+(47, 'Traditional Sculpting', 'DA211', 0, 3),
+(48, 'Digital Publishing – II', 'DG200', 0, 3),
+(49, '3D Digital Visualization', 'DA311', 0, 3),
+(50, 'Psychology in Art and Design', 'DA321', 0, 3),
+(53, 'Sociology in Art and Design', 'DV200', 0, 3);
 
 -- --------------------------------------------------------
 
@@ -126,11 +204,26 @@ INSERT INTO `periode` (`id`, `tahun_ajar`, `semester`, `flag`) VALUES
 
 CREATE TABLE IF NOT EXISTS `prodi` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
+  `fakultas_id` int(10) NOT NULL,
   `prodi_name` varchar(50) NOT NULL,
   `prodi_code` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `prodi_code` (`prodi_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  UNIQUE KEY `prodi_code` (`prodi_code`),
+  KEY `FK_prodi_fakultas2` (`fakultas_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+
+--
+-- Dumping data for table `prodi`
+--
+
+INSERT INTO `prodi` (`id`, `fakultas_id`, `prodi_name`, `prodi_code`) VALUES
+(1, 1, 'Manajemen', 'EM'),
+(2, 1, 'Akuntansi', 'EA'),
+(3, 2, 'Sistem Informasi', 'SI'),
+(4, 2, 'Teknik Informatika', 'TI'),
+(14, 2, 'Sistem Komputer', 'SK'),
+(15, 3, 'Ilmu Komunikasi', 'IK'),
+(16, 4, 'Desain Komunikasi Visual', 'DKV');
 
 -- --------------------------------------------------------
 
@@ -251,6 +344,12 @@ CREATE TABLE IF NOT EXISTS `trx_jadwal_requirement` (
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `prodi`
+--
+ALTER TABLE `prodi`
+  ADD CONSTRAINT `FK_prodi_fakultas2` FOREIGN KEY (`fakultas_id`) REFERENCES `fakultas` (`id`);
 
 --
 -- Constraints for table `ruang_kelas`
