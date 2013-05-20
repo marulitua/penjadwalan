5ce097b2bb90c960337333be95c0ff23c8ee5fbb
erwinmaruli <erwinmaruli@live>
2013-05-21 03:46:15 +0700

    Add Dosen and Matakuliah

--- protected/controllers/MataKuliahController.php
+++ protected/controllers/MataKuliahController.php
@@ -1,176 +1,177 @@
 <?php
 
-class MataKuliahController extends Controller
-{
-	/**
-	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
-	 * using two-column layout. See 'protected/views/layouts/column2.php'.
-	 */
-	public $layout='//layouts/column2';
+class MataKuliahController extends Controller {
 
-	/**
-	 * @return array action filters
-	 */
-	public function filters()
-	{
-		return array(
-			'accessControl', // perform access control for CRUD operations
-		);
-	}
+    /**
+     * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+     * using two-column layout. See 'protected/views/layouts/column2.php'.
+     */
+    public $layout = '//layouts/column2';
 
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
-	public function accessRules()
-	{
-		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
-		);
-	}
+    /**
+     * @return array action filters
+     */
+    public function filters() {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+        );
+    }
 
-	/**
-	 * Displays a particular model.
-	 * @param integer $id the ID of the model to be displayed
-	 */
-	public function actionView($id)
-	{
-		$this->render('view',array(
-			'model'=>$this->loadModel($id),
-		));
-	}
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules() {
+        return array(
+            array('allow', // allow all users to perform 'index' and 'view' actions
+                'actions' => array('index', 'view'),
+                'users' => array('*'),
+            ),
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'actions' => array('create', 'update'),
+                'users' => array('@'),
+            ),
+            array('allow', // allow admin user to perform 'admin' and 'delete' actions
+                'actions' => array('admin', 'delete'),
+                'users' => array('admin'),
+            ),
+            array('deny', // deny all users
+                'users' => array('*'),
+            ),
+        );
+    }
 
-	/**
-	 * Creates a new model.
-	 * If creation is successful, the browser will be redirected to the 'view' page.
-	 */
-	public function actionCreate()
-	{
-		$model=new MataKuliah;
+    /**
+     * Displays a particular model.
+     * @param integer $id the ID of the model to be displayed
+     */
+    public function actionView($id) {
+        $this->render('view', array(
+            'model' => $this->loadModel($id),
+        ));
+    }
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+    /**
+     * Creates a new model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     */
+    public function actionCreate() {
+        $model = new MataKuliah;
 
-		if(isset($_POST['MataKuliah']))
-		{
-			$model->attributes=$_POST['MataKuliah'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
-		}
+        // Uncomment the following line if AJAX validation is needed
+        $this->performAjaxValidation($model);
 
-		$this->render('create',array(
-			'model'=>$model,
-		));
-	}
+        if (isset($_POST['MataKuliah'])) {
+            $model->attributes = $_POST['MataKuliah'];
 
-	/**
-	 * Updates a particular model.
-	 * If update is successful, the browser will be redirected to the 'view' page.
-	 * @param integer $id the ID of the model to be updated
-	 */
-	public function actionUpdate($id)
-	{
-		$model=$this->loadModel($id);
+            if ($model->praktek == 1) {
+                $model2 = new MataKuliah;
+                $model2->attributes = $model->attributes;
+                $model2->mata_kuliah_code .= 'P';
+                $model2->sks = $_POST['MataKuliah']['sksPraktek'];
+                
+                //belong to teori
+                $model->praktek = 0;
+                $model->sks = $_POST['MataKuliah']['sksTeori'];
+                
+                if ($model->save() && $model2->save())
+                    $this->redirect(array('view', 'id' => $model->id));
+            } else
+            if ($model->save())
+                $this->redirect(array('view', 'id' => $model->id));
+        }
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+        $this->render('create', array(
+            'model' => $model,
+        ));
+    }
 
-		if(isset($_POST['MataKuliah']))
-		{
-			$model->attributes=$_POST['MataKuliah'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
-		}
+    /**
+     * Updates a particular model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id the ID of the model to be updated
+     */
+    public function actionUpdate($id) {
+        $model = $this->loadModel($id);
 
-		$this->render('update',array(
-			'model'=>$model,
-		));
-	}
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-	/**
-	 * Deletes a particular model.
-	 * If deletion is successful, the browser will be redirected to the 'admin' page.
-	 * @param integer $id the ID of the model to be deleted
-	 */
-	public function actionDelete($id)
-	{
-		if(Yii::app()->request->isPostRequest)
-		{
-			// we only allow deletion via POST request
-			$this->loadModel($id)->delete();
+        if (isset($_POST['MataKuliah'])) {
+            $model->attributes = $_POST['MataKuliah'];
+            if ($model->save())
+                $this->redirect(array('view', 'id' => $model->id));
+        }
 
-			// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-			if(!isset($_GET['ajax']))
-				$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-		}
-		else
-			throw new CHttpException(400,'Invalid request. Please do not repeat this request again.');
-	}
+        $this->render('update', array(
+            'model' => $model,
+        ));
+    }
 
-	/**
-	 * Lists all models.
-	 */
-	public function actionIndex()
-	{
-		$dataProvider=new CActiveDataProvider('MataKuliah');
-		$this->render('index',array(
-			'dataProvider'=>$dataProvider,
-		));
-	}
+    /**
+     * Deletes a particular model.
+     * If deletion is successful, the browser will be redirected to the 'admin' page.
+     * @param integer $id the ID of the model to be deleted
+     */
+    public function actionDelete($id) {
+        if (Yii::app()->request->isPostRequest) {
+            // we only allow deletion via POST request
+            $this->loadModel($id)->delete();
 
-	/**
-	 * Manages all models.
-	 */
-	public function actionAdmin()
-	{
-		$model=new MataKuliah('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['MataKuliah']))
-			$model->attributes=$_GET['MataKuliah'];
+            // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+            if (!isset($_GET['ajax']))
+                $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+        }
+        else
+            throw new CHttpException(400, 'Invalid request. Please do not repeat this request again.');
+    }
 
-		$this->render('admin',array(
-			'model'=>$model,
-		));
-	}
+    /**
+     * Lists all models.
+     */
+    public function actionIndex() {
+        $dataProvider = new CActiveDataProvider('MataKuliah');
+        $this->render('index', array(
+            'dataProvider' => $dataProvider,
+        ));
+    }
 
-	/**
-	 * Returns the data model based on the primary key given in the GET variable.
-	 * If the data model is not found, an HTTP exception will be raised.
-	 * @param integer the ID of the model to be loaded
-	 */
-	public function loadModel($id)
-	{
-		$model=MataKuliah::model()->findByPk($id);
-		if($model===null)
-			throw new CHttpException(404,'The requested page does not exist.');
-		return $model;
-	}
+    /**
+     * Manages all models.
+     */
+    public function actionAdmin() {
+        $model = new MataKuliah('search');
+        $model->unsetAttributes();  // clear any default values
+        if (isset($_GET['MataKuliah']))
+            $model->attributes = $_GET['MataKuliah'];
 
-	/**
-	 * Performs the AJAX validation.
-	 * @param CModel the model to be validated
-	 */
-	protected function performAjaxValidation($model)
-	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='mata-kuliah-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-	}
+        $this->render('admin', array(
+            'model' => $model,
+        ));
+    }
+
+    /**
+     * Returns the data model based on the primary key given in the GET variable.
+     * If the data model is not found, an HTTP exception will be raised.
+     * @param integer the ID of the model to be loaded
+     */
+    public function loadModel($id) {
+        $model = MataKuliah::model()->findByPk($id);
+        if ($model === null)
+            throw new CHttpException(404, 'The requested page does not exist.');
+        return $model;
+    }
+
+    /**
+     * Performs the AJAX validation.
+     * @param CModel the model to be validated
+     */
+    protected function performAjaxValidation($model) {
+        if (isset($_POST['ajax']) && $_POST['ajax'] === 'mata-kuliah-form') {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+    }
+
 }
--- protected/views/mataKuliah/_form.php
+++ protected/views/mataKuliah/_form.php
@@ -1,28 +1,68 @@
-<?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
-	'id'=>'mata-kuliah-form',
-	'enableAjaxValidation'=>false,
-)); ?>
+<script type="text/javascript">
 
-	<p class="help-block">Fields with <span class="required">*</span> are required.</p>
+    $("#MataKuliah_praktek").live('click', function() {
 
-	<?php echo $form->errorSummary($model); ?>
+        if ($("#areaSks").hasClass("hidden"))
+        {
+            $("#areaSks").removeClass("hidden")
+            $("#mandatorySks").addClass("hidden")
+        }
+        else {
+            $("#areaSks").addClass("hidden");
+            $("#mandatorySks").removeClass("hidden");
+        }
+        
+        $("#MataKuliah_sks").val(0);
+    });
 
-	<?php echo $form->textFieldRow($model,'mata_kuliah',array('class'=>'span5','maxlength'=>50)); ?>
 
-	<?php echo $form->textFieldRow($model,'mata_kuliah_code',array('class'=>'span5','maxlength'=>50)); ?>
+</script>
 
-        <?php echo $form->labelEx($model, 'praktek'); ?>
-        <?php echo $form->checkBox($model, 'praktek',array('class'=>'span5')); ?>
-        <?php echo $form->error($model, 'praktek'); ?>
+<?php
+//Yii::app()->clientScript->registerScript('$("#MataKuliah_praktek").click(function (){ if($("#areaSks").style("hide") == true)  };)');
 
-	<?php echo $form->textFieldRow($model,'sks',array('class'=>'span5')); ?>
 
-	<div class="form-actions">
-		<?php $this->widget('bootstrap.widgets.TbButton', array(
-			'buttonType'=>'submit',
-			'type'=>'primary',
-			'label'=>$model->isNewRecord ? 'Create' : 'Save',
-		)); ?>
-	</div>
+
+$form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+    'id' => 'mata-kuliah-form',
+    'enableAjaxValidation' => true,
+        ));
+?>
+
+<p class="help-block">Fields with <span class="required">*</span> are required.</p>
+
+<?php echo $form->errorSummary($model); ?>
+
+<?php echo $form->textFieldRow($model, 'mata_kuliah', array('class' => 'span5', 'maxlength' => 50)); ?>
+
+<?php echo $form->textFieldRow($model, 'mata_kuliah_code', array('class' => 'span5', 'maxlength' => 50)); ?>
+
+<?php echo $form->labelEx($model, 'praktek'); ?>
+<?php echo $form->checkBox($model, 'praktek', array('class' => 'span5')); ?>
+<?php echo $form->error($model, 'praktek'); ?>
+
+<div id="mandatorySks">
+    <?php echo $form->textFieldRow($model, 'sks', array('class' => 'span5')); ?>
+</div>
+
+<div id="areaSks" class="well hidden">
+    <?php
+    echo CHtml::label('Teori', 'sksTeori');
+    echo CHtml::textField('MataKuliah[sksTeori]', 2);
+    echo CHtml::label('Praktek', 'sksPraktek');
+    echo CHtml::textField('MataKuliah[sksPraktek]', 1);
+    ?> 
+</div>
+
+
+<div class="form-actions">
+    <?php
+    $this->widget('bootstrap.widgets.TbButton', array(
+        'buttonType' => 'submit',
+        'type' => 'primary',
+        'label' => $model->isNewRecord ? 'Create' : 'Save',
+    ));
+    ?>
+</div>
 
 <?php $this->endWidget(); ?>
